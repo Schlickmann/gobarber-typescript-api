415354f86de54aecf7af1b4ce427c7e6bd5594af
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-04-29 21:47:04 -0700

    added debugging configuration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,12 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello Go Juliani' }),
-);
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = { name, email };
+
+  return response.json(user);
+});
 
 export default routes;
