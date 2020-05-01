2b9ab2344c0c52abd3354c0a72d8eefdcdc4d939
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-04-30 18:56:20 -0700

    added validation to post method in appointments routes

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,14 +1,34 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+// Defining type of appointments array
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const appointment = { id: uuid(), provider, date };
+  const parsedDate = startOfHour(parseISO(date));
+
+  const isDateNotAvailable = appointments.find(appointment =>
+    isEqual(appointment.date, parsedDate),
+  );
+
+  if (isDateNotAvailable) {
+    return response.status(401).json({
+      error: 'Operation not permitted. Date and time select is already booked.',
+    });
+  }
+
+  const appointment = { id: uuid(), provider, date: parsedDate };
 
   appointments.push(appointment);
 
