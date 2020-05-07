1f864b79be85968f33770fb38630666bef25b1be
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-06 21:07:02 -0700

    added jwt authentication

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -10,11 +10,11 @@ sessionsRouter.post('/', async (request, response) => {
 
     const createSession = new CreateSessionService();
 
-    const { user } = await createSession.execute({ email, password });
+    const { user, token } = await createSession.execute({ email, password });
 
     delete user.password;
 
-    return response.json({ user });
+    return response.json({ user, token });
   } catch (error) {
     return response.status(400).json({ error: error.message });
   }
--- src/services/CreateSessionService.ts
+++ src/services/CreateSessionService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,24 +11,30 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 export default class CreateSessionService {
   public async execute({ password, email }: Request): Promise<Response> {
     const userRepository = getRepository(User);
 
-    const userExists = await userRepository.findOne({ where: { email } });
+    const user = await userRepository.findOne({ where: { email } });
 
-    if (!userExists) {
+    if (!user) {
       throw Error('Incorrect email/password combination.');
     }
 
-    const passwordMatched = await compare(password, userExists.password);
+    const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
       throw Error('Incorrect email/password combination.');
     }
 
-    return { user: userExists };
+    const token = sign({}, '12f7e8ea8c0c6266d00b2bc8993680f7', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
