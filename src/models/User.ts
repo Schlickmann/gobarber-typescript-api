6c5c8cea44b99b223043c342fb416aa96adbd720
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-06 18:54:41 -0700

    created users table

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,16 +7,19 @@ import {
 } from 'typeorm';
 
 // Creates relation between model and table
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider: string;
+  name: string;
 
-  @Column('timestamp with time zone')
-  date: Date;
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   @CreateDateColumn()
   created_at: Date;
@@ -25,4 +28,4 @@ class Appointment {
   updated_at: Date;
 }
 
-export default Appointment;
+export default User;
