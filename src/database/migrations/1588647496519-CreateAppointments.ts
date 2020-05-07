ab78d014ce141f4fb411989fd0658c3a7bdd0831
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-06 19:14:54 -0700

    added relation between providers and appointments

--- src/database/migrations/1588647496519-CreateAppointments.ts
+++ src/database/migrations/1588647496519-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1588647496519
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1588815567303-CreateUsers.ts
+++ src/database/migrations/1588815567303-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1588815567303 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
