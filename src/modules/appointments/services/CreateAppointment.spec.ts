3d90ceb6a9e4d67f0c18fa6d1984bd917fbf5ca2
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-11-07 19:02:28 -0800

    added tests for create appointment service

--- src/modules/appointments/services/CreateAppointment.spec.ts
+++ src/modules/appointments/services/CreateAppointment.spec.ts
@@ -1,11 +1,32 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('Appointments Services', () => {
   describe('CreateAppointment', () => {
-    it('should create a new appointment', () => {});
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
 
-    it('should not create two appointments in the same date/time', () => {
-      expect(1 + 1).toBe(2);
+    it('should create a new appointment', async () => {
+      const appointment = await createAppointment.execute({
+        date: new Date('12/12/2020'),
+        provider_id: '232321321312',
+      });
+
+      expect(appointment).toHaveProperty('id');
+      expect(appointment.provider_id).toBe('232321321312');
+    });
+
+    it('should not create two appointments in the same date/time', async () => {
+      const appointment = await createAppointment.execute({
+        date: new Date('12/12/2020'),
+        provider_id: '232321321312',
+      });
+
+      expect(appointment).toThrow(
+        'Operation not permitted. Date and time select is already booked.',
+      );
     });
   });
 });
