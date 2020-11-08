5d4f09a6b95bb364e2194a15b4ed527ce3766d9e
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-11-07 18:13:32 -0800

    added jest to app

---
+++ src/modules/appointments/services/CreateAppointment.spec.ts
@@ -0,0 +1,11 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('Appointments Services', () => {
+  describe('CreateAppointment', () => {
+    it('should create a new appointment', () => {});
+
+    it('should not create two appointments in the same date/time', () => {
+      expect(1 + 1).toBe(2);
+    });
+  });
+});
